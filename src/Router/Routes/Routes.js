c70faf1555f44a23488bbdb59bba57ed66013cc3
mdshahjhan <mdshahjha478877@gmail.com>
2023-04-08 11:57:54 +0600

    git push

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,8 +1,18 @@
+import Main from "../../Layout/Main";
+import Home from "../../pages/Home/Home";
+
 const { createBrowserRouter } = require("react-router-dom");
 
 const router = createBrowserRouter([
     {
-      path:"/"
+      path:"/",
+      element:<Main></Main>,
+      children:[
+        {
+          path:"/",
+          element:<Home></Home>
+        }
+      ]
     }
   ])
   export default router;
